d41042405eb30e004915301fdff30afa1f63b355
Iago Sousa <sousair2001@gmail.com>
2023-07-28 15:16:25 -0300

    feat: add save method on UserRepository Interface

--- src/application/adapters/repositories/user.ts
+++ src/application/adapters/repositories/user.ts
@@ -1,6 +1,9 @@
+import { InternalError } from '@application/errors/internal-error';
 import { Email } from '@entities/email';
 import { User } from '@entities/user';
+import { Either } from '@utils/either';
 
 export interface IUserRepository {
   findOneByEmail(email: Email): Promise<User | null>;
+  save(user: User): Promise<Either<InternalError, number>>;
 }
