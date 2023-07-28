73bff55c74b4f1f6accc81fb8cd6163040dab07d
Iago Sousa <sousair2001@gmail.com>
2023-07-28 11:53:32 -0300

    test: either test

---
+++ src/utils/tests/either.spec.ts
@@ -0,0 +1,42 @@
+import { failure, Failure, success, Success } from '@utils/either';
+
+describe('Either', () => {
+  type TestFailure = { failure: string };
+  type TestSuccess = { success: string };
+
+  it('failure function should return a failure instance', () => {
+    const result = failure<TestFailure, TestSuccess>({ failure: '' });
+
+    expect(result).toBeInstanceOf(Failure);
+  });
+
+  it('success function should return a success instance', () => {
+    const result = success<TestFailure, TestSuccess>({ success: '' });
+
+    expect(result).toBeInstanceOf(Success);
+  });
+
+  it('isFailure function should return true on a failure instance', () => {
+    const failure = new Failure<TestFailure, TestSuccess>({ failure: '' });
+
+    expect(failure.isFailure()).toBeTruthy();
+  });
+
+  it('isFailure function should return false on a success instance', () => {
+    const success = new Success<TestFailure, TestSuccess>({ success: '' });
+
+    expect(success.isFailure()).toBeFalsy();
+  });
+
+  it('isSuccess function should return false on a failure instance', () => {
+    const failure = new Failure<TestFailure, TestSuccess>({ failure: '' });
+
+    expect(failure.isSuccess()).toBeFalsy();
+  });
+
+  it('isSuccess function should return true on a success instance', () => {
+    const success = new Success<TestFailure, TestSuccess>({ success: '' });
+
+    expect(success.isSuccess()).toBeTruthy();
+  });
+});
