c19e4dc90a8e6bbc57dc2266f8bd523823122869
Iago Sousa <sousair2001@gmail.com>
2023-07-28 12:46:39 -0300

    test: should return true when valid user data is sent

--- src/domain/entities/tests/user.spec.ts
+++ src/domain/entities/tests/user.spec.ts
@@ -1,6 +1,17 @@
+import { Email } from '@entities/email';
 import { User } from '@entities/user';
 
 describe('User Entity', () => {
+  const validUser = <User>{
+    id: 'id',
+    email: new Email('validEmail@domain.com'),
+    emailValidated: false,
+    hashedPassword: 'Pa55w0rd',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  };
+
   describe('validate', () => {
     it('should return false when undefined is sent', () => {
       const result = User.validate(undefined);
@@ -12,6 +23,12 @@ describe('User Entity', () => {
       const result = User.validate(<User>{});
 
       expect(result).toStrictEqual(false);
+    });
+
+    it('should return true when sent an valid user', () => {
+      const result = User.validate(validUser);
+
+      expect(result).toStrictEqual(true);
     });
   });
 });
