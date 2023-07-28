04df131d49b8dab7450e97a02cf884bc5cfdca40
Iago Sousa <sousair2001@gmail.com>
2023-07-28 11:49:39 -0300

    feat: add User entity

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,26 @@
+import { InvalidUserError } from '@domain/errors/invalid-user';
+import { Email } from '@entities/email';
+import { Either, success } from '@utils/either';
+
+export class User {
+  id: string;
+  email: Email;
+  emailValidated: boolean;
+  hashedPassword: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+
+  constructor(user: User) {
+    Object.assign(this, user);
+  }
+
+  static create(user: User): Either<InvalidUserError, User> {
+    return success(new User(user));
+  }
+
+  static validate(user: User): boolean {
+    return true;
+  }
+}
