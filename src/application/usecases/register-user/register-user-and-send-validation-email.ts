2c6584d98cef890d0ba1e4385cfd1b7ee4927695
Iago Sousa <sousair2001@gmail.com>
2023-07-28 14:28:18 -0300

    feat: hash user password

--- src/application/usecases/register-user/register-user-and-send-validation-email.ts
+++ src/application/usecases/register-user/register-user-and-send-validation-email.ts
@@ -3,16 +3,24 @@ import { UserAlreadyRegisteredError } from '@application/errors/user-already-reg
 import { User } from '@entities/user';
 import { failure, success } from '@utils/either';
 import { IRegisterUser } from './register-user';
+import { IEncrypterProvider } from '@application/adapters/providers/encrypter';
 
 export class RegisterUserAndSendValidationEmail implements IRegisterUser {
-  constructor(private readonly userRepository: IUserRepository) {}
+  constructor(private readonly userRepository: IUserRepository, private readonly encrypter: IEncrypterProvider) {}
 
-  async register({ email }: IRegisterUser.Params): IRegisterUser.Result {
-    const userEmailAlreadyRegistered = this.userRepository.findOneByEmail(email);
+  async register({ email, password }: IRegisterUser.Params): IRegisterUser.Result {
+    const userEmailAlreadyRegistered = await this.userRepository.findOneByEmail(email);
 
     if (userEmailAlreadyRegistered) {
       return failure(new UserAlreadyRegisteredError());
     }
+
+    const encrypterRes = this.encrypter.encryptUserPassword(password.value);
+
+    if (encrypterRes.isFailure()) {
+      return failure(encrypterRes.value);
+    }
+
     return success(new User(undefined));
   }
 }
