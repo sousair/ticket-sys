726cd42c8321a983a7f38eae61f2b660b614b322
Iago Sousa <sousair2001@gmail.com>
2023-07-28 14:15:16 -0300

    feat: check user email already registered

--- src/application/usecases/register-user/register-user-and-send-validation-email.ts
+++ src/application/usecases/register-user/register-user-and-send-validation-email.ts
@@ -1,10 +1,18 @@
+import { IUserRepository } from '@application/adapters/repositories/user';
+import { UserAlreadyRegisteredError } from '@application/errors/user-already-registered';
 import { User } from '@entities/user';
-import { success } from '@utils/either';
+import { failure, success } from '@utils/either';
 import { IRegisterUser } from './register-user';
 
 export class RegisterUserAndSendValidationEmail implements IRegisterUser {
+  constructor(private readonly userRepository: IUserRepository) {}
 
   async register({ email }: IRegisterUser.Params): IRegisterUser.Result {
+    const userEmailAlreadyRegistered = this.userRepository.findOneByEmail(email);
+
+    if (userEmailAlreadyRegistered) {
+      return failure(new UserAlreadyRegisteredError());
+    }
     return success(new User(undefined));
   }
 }
