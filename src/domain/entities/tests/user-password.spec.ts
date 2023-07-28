388baf2adba7906f9d26b525a084f59017801394
Iago Sousa <sousair2001@gmail.com>
2023-07-28 10:25:41 -0300

    test: should return false when sent a password without at least one digit

--- src/domain/entities/tests/user-password.spec.ts
+++ src/domain/entities/tests/user-password.spec.ts
@@ -45,5 +45,13 @@ describe('UserPassword Entity', () => {
 
       expect(result).toStrictEqual(false);
     });
+
+    it('should return false when sent a password without at least one digit', () => {
+      const invalidPassword = 'Inval!dPass';
+
+      const result = UserPassword.validate(invalidPassword);
+
+      expect(result).toStrictEqual(false);
+    });
   });
 });
