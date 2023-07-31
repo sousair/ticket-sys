2461583c23f798c23e4f1c2f8dd4675b50763f1d
Iago Sousa <sousair2001@gmail.com>
2023-07-31 20:24:12 -0300

    test: should return failure when token is expired

--- src/application/usecases/validate-user-email/tests/validate-user-email-and-emit-event.spec.ts
+++ src/application/usecases/validate-user-email/tests/validate-user-email-and-emit-event.spec.ts
@@ -5,16 +5,24 @@ import { Failure, failure, success } from '@utils/either';
 import { MINUTE_IN_MS } from '@utils/time';
 import { IValidateUserEmail } from '../validate-user-email';
 import { ValidateUserEmailAndEmitEvent } from '../validate-user-email-and-emit-event';
+import { TokenExpiredError } from '@application/errors/token-expired';
 
 describe('ValidateUserEmailAndEmitEvent UseCase', () => {
   const mockedDate = new Date();
   jest.useFakeTimers().setSystemTime(mockedDate.getTime());
-  
+
   let validParams: IValidateUserEmail.Params;
 
   let sut: ValidateUserEmailAndEmitEvent;
 
   let tokenProvider: ITokenProvider;
+
+  const mockedTokenValidateRes: ITokenProvider.ValidateTokenResData = {
+    payload: {
+      userId: 'anyUserId',
+    },
+    expirationDate: new Date(mockedDate.getTime() + 3 * MINUTE_IN_MS),
+  };
 
   beforeEach(() => {
     class TokenProviderStub implements ITokenProvider {
@@ -23,12 +31,7 @@ describe('ValidateUserEmailAndEmitEvent UseCase', () => {
       }
 
       validateToken(): ITokenProvider.ValidateTokenResult {
-        return success({
-          payload: {
-            userId: 'anyUserId',
-          },
-          expirationDate: new Date(mockedDate.getTime() + 3 * MINUTE_IN_MS),
-        });
+        return success(mockedTokenValidateRes);
       }
     }
 
@@ -55,7 +58,7 @@ describe('ValidateUserEmailAndEmitEvent UseCase', () => {
 
   it('should return Failure and InvalidTokenError when TokenProvider.validateToken returns a Failure with InvalidTokenError', async () => {
     jest.spyOn(tokenProvider, 'validateToken').mockReturnValueOnce(failure(new InvalidTokenError()));
-    
+
     const result = await sut.validate(validParams);
 
     expect(result).toBeInstanceOf(Failure);
@@ -64,10 +67,24 @@ describe('ValidateUserEmailAndEmitEvent UseCase', () => {
 
   it('should return Failure and InternalError when TokenProvider.validateToken returns a Failure with InternalError', async () => {
     jest.spyOn(tokenProvider, 'validateToken').mockReturnValueOnce(failure(new InternalError('error validating token')));
-    
+
     const result = await sut.validate(validParams);
 
     expect(result).toBeInstanceOf(Failure);
     expect(result.value).toBeInstanceOf(InternalError);
+  });
+
+  it('should return Failure and TokenExpiredError when TokenProvider.validateToken returns a expired date', async () => {
+    jest.spyOn(tokenProvider, 'validateToken').mockReturnValueOnce(
+      success({
+        ...mockedTokenValidateRes,
+        expirationDate: new Date(mockedDate.getTime() - 3 * MINUTE_IN_MS),
+      })
+    );
+
+    const result = await sut.validate(validParams);
+
+    expect(result).toBeInstanceOf(Failure);
+    expect(result.value).toBeInstanceOf(TokenExpiredError);
   });
 });
