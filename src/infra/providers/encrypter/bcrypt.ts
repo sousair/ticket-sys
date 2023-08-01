569ca5aae4f5587fc464a080487dda4a3d1b46c7
Iago Sousa <sousair2001@gmail.com>
2023-08-01 15:15:27 -0300

    feat: implements bcrypt encrypter provider

---
+++ src/infra/providers/encrypter/bcrypt.ts
@@ -0,0 +1,25 @@
+import { IEncrypterProvider } from '@application/adapters/providers/encrypter';
+import { InternalError } from '@application/errors/internal-error';
+import { UserPassword } from '@entities/user-password';
+import { Either, failure, success } from '@utils/either';
+import bcrypt from 'bcrypt';
+
+export class BcryptEncrypterProvider implements IEncrypterProvider {
+  constructor(private readonly userPasswordSaltRounds: number) {}
+
+  encryptUserPassword(userPassword: UserPassword): Either<InternalError, string> {
+
+    try {
+      const hashedPassword = bcrypt.hashSync(userPassword.value, this.userPasswordSaltRounds);
+
+      if (!hashedPassword) {
+        throw new Error(`Bcrypt returned an empty or invalid hash: ${hashedPassword}`);
+      }
+
+      return success(hashedPassword);
+    } catch (err) {
+      console.error(err);
+      return failure(new InternalError('error hashing user password'));
+    }
+  }
+}
