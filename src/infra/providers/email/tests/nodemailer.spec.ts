ed995f61e9585c17a49e85af85da79a91f04e85f
Iago Sousa <sousair2001@gmail.com>
2023-08-02 03:53:03 -0300

    test: should call nodemailer and return fails when it fails

---
+++ src/infra/providers/email/tests/nodemailer.spec.ts
@@ -0,0 +1,81 @@
+import { IEmailProvider } from '@application/adapters/providers/email';
+import nodemailer from 'nodemailer';
+import { NodemailerProvider } from '../nodemailer';
+import { NodemailerConfigs } from './../nodemailer';
+import { Email } from '@entities/email';
+import { Failure } from '@utils/either';
+import { EmailSendingError } from '@application/errors/email-sending';
+
+jest.mock('nodemailer', () => ({
+  createTransport: jest.fn().mockReturnValue({
+    sendMail: jest.fn().mockResolvedValue({}),
+  }),
+}));
+
+describe('Nodemailer Provider', () => {
+  let sut: NodemailerProvider;
+
+  let validParams: IEmailProvider.Params;
+
+  const mockedNodemailerConfigs: NodemailerConfigs = {
+    host: 'host',
+    port: 1234,
+    user: 'user',
+    password: 'password',
+    from: 'from@domain.com',
+  };
+
+  beforeEach(() => {
+    sut = new NodemailerProvider(mockedNodemailerConfigs);
+
+    validParams = {
+      to: new Email('email@domain.com'),
+      subject: 'subject',
+      html: '<></>',
+      attachments: [
+        {
+          name: 'name',
+          path: 'file/path.extension',
+        },
+      ],
+    };
+  });
+
+  it('should call nodemailer.createTransport with correct values', () => {
+    const nodemailerSpy = jest.spyOn(nodemailer, 'createTransport');
+
+    expect(nodemailerSpy).toHaveBeenCalledTimes(1);
+    expect(nodemailerSpy).toHaveBeenCalledWith({
+      host: mockedNodemailerConfigs.host,
+      port: mockedNodemailerConfigs.port,
+      auth: {
+        user: mockedNodemailerConfigs.user,
+        pass: mockedNodemailerConfigs.password,
+      },
+    });
+  });
+
+  it('should call nodemailer.sendMail with correct values', async () => {
+    const nodemailerSpy = jest.spyOn(nodemailer.createTransport(), 'sendMail');
+
+    await sut.sendMail(validParams);
+
+    expect(nodemailerSpy).toHaveBeenCalledTimes(1);
+    expect(nodemailerSpy).toHaveBeenCalledWith({
+      from: mockedNodemailerConfigs.from,
+      ...validParams,
+      to: validParams.to.value,
+    });
+  });
+
+  it('should return Failure and EmailSendingError when nodemailer.sendMail throws', async () => {
+    jest.spyOn(nodemailer.createTransport(), 'sendMail').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const result = await sut.sendMail(validParams);
+
+    expect(result).toBeInstanceOf(Failure);
+    expect(result.value).toBeInstanceOf(EmailSendingError);
+  });
+});
