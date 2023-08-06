758acbf42e388d7ae3df08a7c5b9ba07346a3e94
Iago Sousa <sousair2001@gmail.com>
2023-08-06 20:46:42 -0300

    feat: map register user use case failure errors to status and messages

--- src/presentation/http/controllers/register-user.ts
+++ src/presentation/http/controllers/register-user.ts
@@ -1,3 +1,4 @@
+import { IRegisterUser } from '@application/usecases/register-user/register-user';
 import { Email } from '@entities/email';
 import { User } from '@entities/user';
 import { UserPassword } from '@entities/user-password';
@@ -15,7 +16,42 @@ export namespace RegisterUserController {
   };
 }
 
+type RegisterUserErrorStatus = {
+  [key: string]: {
+    status: HttpStatusCode;
+    message: string;
+  };
+  default: {
+    status: HttpStatusCode;
+    message: string;
+  };
+};
+
 export class RegisterUserController implements IController<RegisterUserController.Params, RegisterUserController.ResultData> {
+  private readonly registerUserErrorStatus: RegisterUserErrorStatus = {
+    InvalidUser: {
+      status: HttpStatusCode.BAD_REQUEST,
+      message: 'invalid user info',
+    },
+    InvalidEmail: {
+      status: HttpStatusCode.BAD_REQUEST,
+      message: 'invalid email',
+    },
+    UserAlreadyRegistered: {
+      status: HttpStatusCode.CONFLICT,
+      message: 'email already registered',
+    },
+    InternalError: {
+      status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+      message: 'internal server error',
+    },
+    default: {
+      status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+      message: 'internal server error',
+    },
+  };
+  constructor(private readonly registerUser: IRegisterUser) {}
+
   async handle({ email, password }: RegisterUserController.Params): Promise<IControllerResponse<RegisterUserController.ResultData>> {
     const emailRes = Email.create(email);
     if (emailRes.isFailure()) {
@@ -39,6 +75,20 @@ export class RegisterUserController implements IController<RegisterUserControlle
 
     const emailInstance = emailRes.value;
     const userPassInstance = userPassRes.value;
+
+    const registerUserRes = await this.registerUser.register({ email: emailInstance, password: userPassInstance });
+
+    if (registerUserRes.isFailure()) {
+      console.error(registerUserRes.value);
+      const { status, message } = this.registerUserErrorStatus[registerUserRes.value.name] || this.registerUserErrorStatus.default;
+
+      return {
+        status,
+        data: {
+          message,
+        },
+      };
+    }
 
     return;
   }
