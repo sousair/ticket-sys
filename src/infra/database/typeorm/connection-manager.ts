588e3abd21219beebd75f72d335167dd3233bff7
Iago Sousa <sousair2001@gmail.com>
2023-08-06 19:19:51 -0300

    refactor: typeorm connection manager

--- src/infra/database/typeorm/connection-manager.ts
+++ src/infra/database/typeorm/connection-manager.ts
@@ -10,12 +10,15 @@ export type TypeORMConnectionConfigs = {
   database: string;
 };
 
-export class TypeORMCreateConnection {
-  constructor(private readonly typeORMConnectionConfigs: TypeORMConnectionConfigs) {}
-  async createConnection(): Promise<void> {
-    const { type, host, port, username, password, database } = this.typeORMConnectionConfigs;
-    
-    await new DataSource({
+export class TypeORMConnectionManager {
+  static dataSource: DataSource;
+
+  static async getDataSource(typeORMConnectionConfigs: TypeORMConnectionConfigs): Promise<DataSource> {
+    if (this.dataSource) return this.dataSource;
+
+    const { type, host, port, username, password, database } = typeORMConnectionConfigs;
+
+    const dataSource = new DataSource({
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       type: <any>type,
       host,
@@ -27,6 +30,11 @@ export class TypeORMCreateConnection {
       entities: [join(__dirname, '/entities/**/*{.ts,.js}')],
       migrations: [join(__dirname, '/migration/**/*{.ts,.js}')],
       migrationsRun: true,
-    }).initialize();
+    });
+
+    await dataSource.initialize();
+
+    this.dataSource = dataSource;
+    return this.dataSource;
   }
 }
