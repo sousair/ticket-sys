d88d8d0e9e60e859ea05565b5e596c4304b10908
Iago Sousa <sousair2001@gmail.com>
2023-08-07 05:06:05 -0300

    feat: add bcrypt provider factory

--- src/main/configs/env.ts
+++ src/main/configs/env.ts
@@ -1,8 +1,41 @@
-
 export class EnvConfig {
   APP_PORT: string;
 
+  // * Nodemailer configs
+  FROM_EMAIL: string;
+  NODEMAILER_HOST: string;
+  NODEMAILER_PORT: number;
+  NODEMAILER_USER: string;
+  NODEMAILER_USER_PASSWORD: string;
+
+  // * BCrypt configs
+  BCRYPT_PASSWORD_SALT_ROUNTS: number;
+
   static get APP_PORT(): string {
     return process.env.APP_PORT;
+  }
+
+  static get FROM_EMAIL(): string {
+    return process.env.FROM_EMAIL;
+  }
+
+  static get NODEMAILER_HOST(): string {
+    return process.env.NODEMAILER_HOST;
+  }
+
+  static get NODEMAILER_PORT(): number {
+    return Number(process.env.NODEMAILER_PORT);
+  }
+
+  static get NODEMAILER_USER(): string {
+    return process.env.NODEMAILER_USER;
+  }
+
+  static get NODEMAILER_USER_PASSWORD(): string {
+    return process.env.NODEMAILER_USER_PASSWORD;
+  }
+
+  static get BCRYPT_PASSWORD_SALT_ROUNTS(): number {
+    return Number(process.env.BCRYPT_PASSWORD_SALT_ROUNTS);
   }
 }
---
+++ src/main/factories/infra/providers/encrypter/bcrypt.ts
@@ -0,0 +1,13 @@
+import { BcryptEncrypterProvider } from '@infra/providers/encrypter/bcrypt';
+import { EnvConfig } from '@main/configs/env';
+
+export class BcryptEncrypterProviderFactory {
+  static instance: BcryptEncrypterProvider;
+
+  static getInstance(): BcryptEncrypterProvider {
+    if (this.instance) return this.instance;
+
+    this.instance = new BcryptEncrypterProvider(EnvConfig.BCRYPT_PASSWORD_SALT_ROUNTS);
+    return this.instance;
+  }
+}
