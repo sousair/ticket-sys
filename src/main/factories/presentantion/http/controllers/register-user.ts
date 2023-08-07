857d4a331897cae00652e175c240149a558fa2e7
Iago Sousa <sousair2001@gmail.com>
2023-08-07 05:26:22 -0300

    feat: add register user controller

---
+++ src/main/factories/presentantion/http/controllers/register-user.ts
@@ -0,0 +1,14 @@
+import { RegisterUserAndEmitEventFactory } from '@main/factories/application/usecases/register-user-and-emit-event';
+import { RegisterUserController } from 'src/presentation/http/controllers/register-user';
+
+export class RegisterUserControllerFactory {
+  static instance: RegisterUserController;
+
+  static getInstance(): RegisterUserController {
+    if (this.instance) return this.instance;
+
+    this.instance = new RegisterUserController(RegisterUserAndEmitEventFactory.getInstance());
+
+    return this.instance;
+  }
+}
