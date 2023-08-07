5e610160512010a162c5c80e3d1743a425bc8693
Iago Sousa <sousair2001@gmail.com>
2023-08-07 07:00:56 -0300

    feat: connect to db on server file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,15 @@
+import { TypeORMConnectionManager } from '@infra/database/typeorm/connection-manager';
 import { app } from './app';
 import { EnvConfig } from './configs/env';
+
+TypeORMConnectionManager.getDataSource({
+  type: 'mysql',
+  database: EnvConfig.DATABASE_NAME,
+  host: EnvConfig.DATABASE_HOST,
+  port: EnvConfig.DATABASE_PORT,
+  username: EnvConfig.DATABASE_USER,
+  password: EnvConfig.DATABASE_USER_PASSWORD,
+});
 
 app.listen(EnvConfig.APP_PORT, () => {
   console.log(`Server running at port ${EnvConfig.APP_PORT}`);
