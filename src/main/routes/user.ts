c1fa19e8d4e4f401c262b7660dd4911f702375c4
Iago Sousa <sousair2001@gmail.com>
2023-08-09 10:18:13 -0300

    feat: add validate-email route

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,9 +1,11 @@
 import { adaptControllerToExpressRoute } from '@main/adapters/adapt-express-route';
 import { RegisterUserControllerFactory } from '@main/factories/presentantion/http/controllers/register-user';
+import { ValidateUserEmailControllerFactory } from '@main/factories/presentantion/http/controllers/validate-user-email';
 import { Router } from 'express';
 
 const userRouter = Router();
 
 userRouter.post('/', adaptControllerToExpressRoute(RegisterUserControllerFactory.getInstance()));
+userRouter.post('/validate-email/:token', adaptControllerToExpressRoute(ValidateUserEmailControllerFactory.getInstance()));
 
 export { userRouter };
