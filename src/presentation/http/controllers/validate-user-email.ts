720877f503e5a53da5f605864f108ae8e8f0aeab
Iago Sousa <sousair2001@gmail.com>
2023-08-09 10:05:57 -0300

    feat: map validate user email use case failure errors to status and messages

--- src/presentation/http/controllers/validate-user-email.ts
+++ src/presentation/http/controllers/validate-user-email.ts
@@ -1,4 +1,5 @@
 import { IValidateUserEmail } from '@application/usecases/validate-user-email/validate-user-email';
+import { HttpStatusCode } from '@utils/http-status-code';
 import { IController, IControllerResponse } from '@utils/interfaces/controller';
 
 export namespace ValidateUserEmailController {
@@ -11,10 +12,58 @@ export namespace ValidateUserEmailController {
   };
 }
 
+type ValidateUserEmailErrorStatus = {
+  [key: string]: {
+    status: HttpStatusCode;
+    message: string;
+  };
+  default: {
+    status: HttpStatusCode;
+    message: string;
+  };
+};
+
 export class ValidateUserEmailController implements IController<ValidateUserEmailController.Params, ValidateUserEmailController.ResultData> {
+  private readonly validateUserEmailErrorStatus: ValidateUserEmailErrorStatus = {
+    InvalidTokenError: {
+      status: HttpStatusCode.UNAUTHORIZED,
+      message: 'invalid token',
+    },
+    TokenExpiredError: {
+      status: HttpStatusCode.BAD_REQUEST,
+      message: 'expired token',
+    },
+    UserNotFoundError: {
+      status: HttpStatusCode.NOT_FOUND,
+      message: 'user not found',
+    },
+    InternalError: {
+      status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+      message: 'internal server error',
+    },
+    default: {
+      status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+      message: 'internal server error',
+    },
+  };
+
   constructor(private readonly validateUserEmail: IValidateUserEmail) {}
+
   async handle({ token }: ValidateUserEmailController.Params): Promise<IControllerResponse<ValidateUserEmailController.ResultData>> {
     const validateUserEmailRes = await this.validateUserEmail.validate({ token });
+
+    if (validateUserEmailRes.isFailure()) {
+      console.error(validateUserEmailRes.value);
+
+      const { status, message } = this.validateUserEmailErrorStatus[validateUserEmailRes.value.name] || this.validateUserEmailErrorStatus.default;
+
+      return {
+        status,
+        data: {
+          message,
+        },
+      };
+    }
 
     return;
   }
