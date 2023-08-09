827a8a591c4ac789653202d058aa698bd740fe6f
Iago Sousa <sousair2001@gmail.com>
2023-08-09 10:19:46 -0300

    fix: presentation folder name on factories

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,6 +1,6 @@
 import { adaptControllerToExpressRoute } from '@main/adapters/adapt-express-route';
-import { RegisterUserControllerFactory } from '@main/factories/presentantion/http/controllers/register-user';
-import { ValidateUserEmailControllerFactory } from '@main/factories/presentantion/http/controllers/validate-user-email';
+import { RegisterUserControllerFactory } from '@main/factories/presentation/http/controllers/register-user';
+import { ValidateUserEmailControllerFactory } from '@main/factories/presentation/http/controllers/validate-user-email';
 import { Router } from 'express';
 
 const userRouter = Router();
