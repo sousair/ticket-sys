3bb7a46022f1d29a08a81e2ced8a74ebe51d01aa
Iago Sousa <sousair2001@gmail.com>
2023-08-14 15:01:13 -0300

    feat: add user login controller

---
+++ src/presentation/http/controllers/login-user.ts
@@ -0,0 +1,98 @@
+import { ILoginUser } from '@application/usecases/login-user/login-user';
+import { Email } from '@entities/email';
+import { HttpStatusCode } from '@shared/http-status-code';
+import { IController, IControllerResponse } from '@shared/interfaces/controller';
+
+export namespace LoginUserController {
+  export type Params = {
+    body: {
+      email: string;
+      password: string;
+    };
+    headers: {
+      'x-forwarded-for': string;
+    };
+  };
+
+  export type ResultData = {
+    token: string;
+  };
+}
+
+type LoginUserErrorStatus = {
+  [key: string]: {
+    status: HttpStatusCode;
+    message: string;
+  };
+  default: {
+    status: HttpStatusCode;
+    message: string;
+  };
+};
+
+export class LoginUserController implements IController<LoginUserController.Params, LoginUserController.ResultData> {
+  private readonly loginUserErrorStatus: LoginUserErrorStatus = {
+    UserNotFoundError: {
+      status: HttpStatusCode.NOT_FOUND,
+      message: 'user not found',
+    },
+    InvalidPasswordError: {
+      status: HttpStatusCode.UNAUTHORIZED,
+      message: 'invalid password',
+    },
+    InternalError: {
+      status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+      message: 'internal server error',
+    },
+    default: {
+      status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+      message: 'internal server error',
+    },
+  };
+
+  constructor(private readonly loginUser: ILoginUser) {}
+
+  async handle({ body, headers }: LoginUserController.Params): Promise<IControllerResponse<LoginUserController.ResultData>> {
+    const { email, password } = body;
+    const { 'x-forwarded-for': ipHeader } = headers;
+
+    const emailRes = Email.create(email);
+    if (emailRes.isFailure()) {
+      return {
+        status: HttpStatusCode.BAD_REQUEST,
+        data: {
+          message: emailRes.value.message,
+        },
+      };
+    }
+
+    const emailInstance = emailRes.value;
+
+    const loginUserRes = await this.loginUser.login({
+      ip: ipHeader,
+      email: emailInstance,
+      password,
+    });
+
+    if (loginUserRes.isFailure()) {
+      const { status, message } = this.loginUserErrorStatus[loginUserRes.value.name] || this.loginUserErrorStatus.default;
+
+      return {
+        status,
+        data: {
+          message,
+        },
+      };
+    }
+
+    const { token } = loginUserRes.value;
+
+    return {
+      status: HttpStatusCode.OK,
+      data: {
+        message: 'login successful',
+        token,
+      },
+    };
+  }
+}
