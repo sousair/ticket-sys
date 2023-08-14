bf462003d69fb40dcb585296a9c7ce7e26325d3e
Iago Sousa <sousair2001@gmail.com>
2023-08-14 15:15:07 -0300

    feat: add user login route

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,4 +1,5 @@
 import { adaptControllerToExpressRoute } from '@main/adapters/adapt-express-route';
+import { LoginUserControllerFactory } from '@main/factories/presentation/http/controllers/login-user';
 import { RegisterUserControllerFactory } from '@main/factories/presentation/http/controllers/register-user';
 import { ValidateUserEmailControllerFactory } from '@main/factories/presentation/http/controllers/validate-user-email';
 import { Router } from 'express';
@@ -7,5 +8,6 @@ const userRouter = Router();
 
 userRouter.post('/', adaptControllerToExpressRoute(RegisterUserControllerFactory.getInstance()));
 userRouter.post('/validate-email/:token', adaptControllerToExpressRoute(ValidateUserEmailControllerFactory.getInstance()));
+userRouter.post('/login', adaptControllerToExpressRoute(LoginUserControllerFactory.getInstance()));
 
 export { userRouter };
