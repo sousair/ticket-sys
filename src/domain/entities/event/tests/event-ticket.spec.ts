e1e734ae1173e561abf1a89bdad0c14400434e2f
Iago Sousa <sousair2001@gmail.com>
2023-08-29 11:12:34 -0300

    feat: test EventTicket.generateTickets

--- src/domain/entities/event/tests/event-ticket.spec.ts
+++ src/domain/entities/event/tests/event-ticket.spec.ts
@@ -1,6 +1,11 @@
 import { Failure, Success } from '@shared/either';
 import { EventTicket } from '../event-ticket';
 import { InvalidEventTicketError } from '@domain/errors/invalid-event-ticket';
+import { Currency } from '@entities/@types/currency.d';
+import { GenerateTicketsDTO } from '../dtos/generate-tickets.dto';
+import { EventTicketType } from '../event-ticket-type';
+import { EventSchedule } from '../event-schedule';
+import { TicketsGenerationError } from '@domain/errors/tickets-generation-error';
 
 describe('EventTicket Entity', () => {
   const validParams: EventTicket = {
@@ -9,6 +14,10 @@ describe('EventTicket Entity', () => {
     totalAmount: 1000,
     boughtAmount: 10,
     scheduleId: 'validScheduleId',
+    price: {
+      currency: Currency.BRL,
+      amount: 49.9,
+    },
     eventTicketTypeId: 'validEventTicketTypeId',
     schedule: null,
     eventTicketType: null,
@@ -63,6 +72,85 @@ describe('EventTicket Entity', () => {
 
       expect(result).toBeInstanceOf(Success);
       expect(result.value).toBeInstanceOf(EventTicket);
+    });
+  });
+
+  describe('generateTickets', () => {
+    let generateParams: GenerateTicketsDTO.Params;
+
+    beforeEach(() => {
+      generateParams = {
+        ticketType: <EventTicketType>{
+          id: 'validEventTicketTypeId',
+          possibleAreaValues: ['A', 'B', 'C'],
+          eventId: 'validEventId',
+          ticketTypeId: 'validTicketTypeId',
+          event: null,
+          ticketType: null,
+        },
+        schedules: [
+          <EventSchedule>{
+            id: 'validScheduleId',
+            date: new Date(),
+            timeZone: -3,
+            eventId: 'validEventId',
+            event: null,
+          },
+        ],
+        tickets: [
+          {
+            locationValue: 'A',
+            totalAmount: 10000,
+            price: {
+              amount: 10,
+              currency: Currency.USD,
+            }
+          }
+        ]
+      };
+    });
+
+    it('should return Failure and TicketsGenerationError when ticketType is undefined', () => {
+      const result = EventTicket.generateTickets({ ...generateParams, ticketType: undefined });
+
+      expect(result).toBeInstanceOf(Failure);
+      expect(result.value).toBeInstanceOf(TicketsGenerationError);
+    });
+
+    it('should return Failure and TicketsGenerationError when schedules is empty', () => {
+      const result = EventTicket.generateTickets({ ...generateParams, schedules: [] });
+
+      expect(result).toBeInstanceOf(Failure);
+      expect(result.value).toBeInstanceOf(TicketsGenerationError);
+    });
+
+    it('should return Failure and TicketsGenerationError when tickets is empty', () => {
+      const result = EventTicket.generateTickets({ ...generateParams, tickets: [] });
+
+      expect(result).toBeInstanceOf(Failure);
+      expect(result.value).toBeInstanceOf(TicketsGenerationError);
+    });
+
+    it('should return Success and an array of EventTickets for valid params', () => {
+      const result = EventTicket.generateTickets(generateParams);
+
+      expect(result).toBeInstanceOf(Success);
+      expect(result.value).toBeInstanceOf(Array<GenerateTicketsDTO.Response>);
+
+      generateParams.tickets.forEach(ticket => {
+        generateParams.schedules.forEach(schedule => {
+          expect(result.value).toContainEqual({
+            locationValue: ticket.locationValue,
+            totalAmount: ticket.totalAmount,
+            boughtAmount: 0,
+            price: ticket.price,
+            scheduleId: schedule.id,
+            schedule,
+            eventTicketTypeId: generateParams.ticketType.id,
+            eventTicketType: generateParams.ticketType,
+          });
+        });
+      });
     });
   });
 });
